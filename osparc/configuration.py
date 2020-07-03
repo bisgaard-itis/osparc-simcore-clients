ae8ab6eceb626dc1552e01e5e1d579209f32b5dd
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-07-03 23:54:51 +0200

    osparc-simcore-python-client package version: 0.3.5 → 0.3.6

--- osparc/configuration.py
+++ osparc/configuration.py
@@ -336,7 +336,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.3.0\n"\
-               "SDK Package Version: 0.3.5".\
+               "SDK Package Version: 0.3.6".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
