8a210f3cd8e554212545b6ffdd0e4cb3b1683426
Mads Bisgaard <126242332+bisgaard-itis@users.noreply.github.com>
2024-06-26 11:01:17 +0200

    ✨ Add download-study-logs-fcn (#163)
    * update workflow before publishing python package
    
    * fix dependency issue and bump version
    
    * point to website in project description
    
    * fix broken dependency
    
    * improve doc
    
    * add github token to download artifacts
    
    * ensure only read-access @wvangeit
    
    * yet another attempt at downloading artifacts
    
    * make sure to use repo that ran the trigger wf
    
    * another attempt at fixing
    
    * change owner
    
    * allow publishing to testpypi also when pr
    
    * minor change
    
    * revert minor (but breaking) change
    
    * minor fix
    
    * add debug messages
    
    * another debug message
    
    * hopefully the final version
    
    * final fix
    
    * minor fix
    
    * move master and tag to individual jobs
    
    * add debug messages
    
    * dev->post
    
    * add python script for determining semantic version
    
    * minor changes
    
    * minor changes
    
    * improve error handling and add version file to artifacts
    
    * check if release
    
    * minor fix
    
    * ensure to enter venv
    
    * also when tagging
    
    * source venv in publishin workflow
    
    * ensure only master
    
    * add script for testing 'pure' semver
    
    * adapt workflows to new python script
    
    * minor change
    
    * attempt to evaluate expressions correctly
    
    * several fixes to fix tests
    
    * ensure repo is checked out in publish workflow
    
    * several small fixes
    
    * cleanup
    
    * debug
    
    * minor cleanup
    
    * mionr changes
    
    * add debug message
    
    * minor change
    
    * minor change
    
    * yet another try
    
    * minor change
    
    * minor change
    
    * minor change
    
    * mionr change
    
    * minor changes
    
    * correct workflow run id
    
    * cosmetic change
    
    * avoid using gh
    
    * change to a single job for publishing
    
    * minor cleanup
    
    * swap loops in clean up jobs
    
    * correction
    
    * update server compatibility to new url
    
    * minor change to trigger ci
    
    * update openapi.json
    
    * update openapi.json
    
    * add ensure_unique_names fcn
    
    * bugfix and improve tsst
    
    * minor fixes
    
    * minor change
    
    * last fixes
    
    * add synchronous version of the method
    
    * @pcrespov private method and default limits+offsets
    
    * add type annotations
    
    * @pcrespov add progressbar when downloading logs
    
    * add more useful log
    
    * minor changes
    
    * add pylint exceptions
    
    * use pytest.fail @pcrespov
    
    * ensure unique file names directly in method
    
    * use Optional instead of |
    
    * remove superfluous file

---
+++ clients/python/test/e2e/test_studies.py
@@ -0,0 +1,63 @@
+# pylint: disable=protected-access
+# pylint: disable=redefined-outer-name
+# pylint: disable=too-many-arguments
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+
+import shutil
+from pathlib import Path
+from uuid import UUID
+
+import osparc
+import pytest
+import tenacity
+from _utils import skip_if_osparc_version
+from packaging.version import Version
+
+
+@skip_if_osparc_version(at_least=Version("0.6.6"))
+@pytest.mark.parametrize("download_dir", [True, False])
+async def test_studies_logs(
+    api_client: osparc.ApiClient,
+    file_with_number: osparc.File,
+    sleeper_study_id: UUID,
+    download_dir: bool,
+    tmp_path: Path,
+):
+    studies_api = osparc.StudiesApi(api_client=api_client)
+    job_inputs = osparc.JobInputs(
+        values={
+            "input_file": file_with_number,
+        }
+    )
+    job = studies_api.create_study_job(
+        study_id=f"{sleeper_study_id}", job_inputs=job_inputs
+    )
+    assert isinstance(job, osparc.Job)
+    print(f"Running study job: {job.id}")
+    status = studies_api.start_study_job(study_id=f"{sleeper_study_id}", job_id=job.id)
+    assert isinstance(status, osparc.JobStatus)
+    async for attempt in tenacity.AsyncRetrying(
+        reraise=True,
+        wait=tenacity.wait_fixed(1),
+        stop=tenacity.stop_after_delay(30),
+        retry=tenacity.retry_if_exception_type(AssertionError),
+    ):
+        with attempt:
+            status = studies_api.inspect_study_job(
+                study_id=f"{sleeper_study_id}", job_id=job.id
+            )
+            assert isinstance(status, osparc.JobStatus)
+            assert status.stopped_at is not None
+    assert status.state == "SUCCESS"
+    try:
+        log_dir = await studies_api.get_study_job_output_logfiles_async(
+            study_id=f"{sleeper_study_id}",
+            job_id=job.id,
+            download_dir=tmp_path if download_dir else None,
+        )
+        assert log_dir.is_dir()
+        n_logfiles = sum(1 for _ in log_dir.rglob("*") if _.is_file())
+        assert n_logfiles > 0
+    finally:
+        shutil.rmtree(log_dir)
