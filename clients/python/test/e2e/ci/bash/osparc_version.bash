1962feff93fbaf814c6fda0013891037176f591f
Mads Bisgaard <126242332+bisgaard-itis@users.noreply.github.com>
2024-10-22 15:18:37 +0200

    ♻️ Fix e2e to use pypi (#197)

---
+++ clients/python/test/e2e/ci/bash/osparc_version.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+
+version=$1
+report=$(mktemp)
+trap 'rm -rf ${report}' ERR
+
+if [ "${version}" == "latest_release" ]; then
+    pip install osparc --force-reinstall --dry-run --quiet --report "${report}"
+elif [ "${version}" == "latest_master" ]; then
+    pip install osparc --pre --force-reinstall --dry-run --quiet --report "${report}"
+else
+    pip install osparc=="${version}" --force-reinstall --dry-run --quiet --report "${report}"
+fi
+jq -r '.install[] | select(.metadata.name == "osparc") | .metadata.version' "${report}"
+rm -rf "${report}"
