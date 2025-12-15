07fd466f17464dd046df274e934f4dee2169b50f
Mads Bisgaard <126242332+bisgaard-itis@users.noreply.github.com>
2025-12-15 15:17:50 +0100

    increase timeout in e2e studies log test (#237)

--- clients/python/test/e2e/test_studies.py
+++ clients/python/test/e2e/test_studies.py
@@ -15,6 +15,8 @@ import tenacity
 from _utils import skip_if_osparc_version
 from packaging.version import Version
 
+_WAIT_TIMEOUT = timedelta(minutes=15)
+
 
 @skip_if_osparc_version(at_least=Version("0.6.6.post7"))
 @pytest.mark.parametrize("download_dir", [True, False])
@@ -35,13 +37,13 @@ async def test_studies_logs(
         study_id=f"{sleeper_study_id}", job_inputs=job_inputs
     )
     assert isinstance(job, osparc.Job)
-    print(f"Running study job: {job.id}")
+    print(job)
     status = studies_api.start_study_job(study_id=f"{sleeper_study_id}", job_id=job.id)
     assert isinstance(status, osparc.JobStatus)
     async for attempt in tenacity.AsyncRetrying(
         reraise=True,
         wait=tenacity.wait_fixed(timedelta(seconds=5)),
-        stop=tenacity.stop_after_delay(timedelta(minutes=10)),
+        stop=tenacity.stop_after_delay(_WAIT_TIMEOUT),
         retry=tenacity.retry_if_exception_type(AssertionError),
     ):
         with attempt:
